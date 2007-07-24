b1fc18474fd8f3f6d278501e7fffeca8528017e6
rowan.hick <rowan.hick@0f55613d-ee35-0410-9de6-1f804e4dffba>
2007-07-24 04:58:14 +0000

    Working through database migration, models

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,6 +1,8 @@
 class Customer < ActiveRecord::Base
   has_many    :agents, :through => :represntation
   has_many    :orders 
+  has_many    :addresses, :through => :addressables
+  has_many    :addressables
   belongs_to  :representation
   belongs_to  :customer_status
   belongs_to  :price_type
