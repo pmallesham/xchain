c10a7760556e330469ccd273c6a0f6a0b5f776a0
rowan.hick <rowan.hick@0f55613d-ee35-0410-9de6-1f804e4dffba>
2007-08-11 16:54:42 +0000

    Adding spec for OrderStatus

--- app/models/order_status.rb
+++ app/models/order_status.rb
@@ -1,6 +1,11 @@
 class OrderStatus < ActiveRecord::Base
+  
   has_many :orders
   attr_accessor :next_statuses
+  
+  def self.find_all
+      find(:all, :order => 'sort_order ASC')
+  end
   
   def self.next_status(current_status)
     if current_status.flow_next?
@@ -10,11 +15,19 @@ class OrderStatus < ActiveRecord::Base
     end
   end
   
-  def find_next_available_statuses()
-    statuses = Array.new
-    statuses << self
-    statuses << OrderStatus.find(:all, :conditions => ["id IN (?)", flow_next], :order => 'sort_order ASC')
-    return statuses
+  def find_next_available_statuses(super_user = false)
+    if !super_user 
+      statuses = Array.new
+      statuses << self
+      if flow_next?
+        statuses << OrderStatus.find(:all, :conditions => ["id IN (?)", flow_next], :order => 'sort_order ASC')
+      else 
+        statuses << OrderStatus.find(:first, :conditions => ["sort_order > ?", sort_order ], :order => 'sort_order ASC')
+      end
+      return statuses
+    else 
+      return OrderStatus.find_all
+    end
   end
   
 end
---
+++ spec/models/order_status_spec.rb
@@ -0,0 +1,48 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe OrderStatus do
+  fixtures :orders, :order_statuses
+    
+  before(:each) do
+    @order_status = OrderStatus.new
+  end
+
+  it "should be able to find standard statuses" do 
+    @order_status = OrderStatus.find(10)
+    @order_status.name.should == 'Draft'
+  end
+end
+
+context OrderStatus, "using a draft order status with no super user" do 
+  
+  before(:each) do 
+    @order_status = OrderStatus.find(10)
+  end
+  
+  it "should have a list of next available status, when flow next is false, only including itself and the next higher status" do
+    s = @order_status.find_next_available_statuses
+    s.size.should == 2
+    s.include?(@order_status).should == true
+    s.include?(OrderStatus.find_by_name('Submitted - Pending Payment')).should == true
+  end
+  
+  it "should not have all statuses available" do
+    s = @order_status.find_next_available_statuses    
+    s.size.should_not == OrderStatus.count
+  end
+  
+end
+
+
+context OrderStatus, "using a draft order status a super user" do 
+  
+  before(:each) do 
+    @order_status = OrderStatus.find(10)
+  end
+  
+  it "should have a list of all available statuses" do
+    s = @order_status.find_next_available_statuses(true)
+    s.size.should == OrderStatus.count
+  end
+  
+end
