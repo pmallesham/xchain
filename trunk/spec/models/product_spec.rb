46ff8fbc205ad51e9615461fd70a3ca80240ee82
rowan.hick <rowan.hick@0f55613d-ee35-0410-9de6-1f804e4dffba>
2007-08-14 01:06:49 +0000

    Adding missing specs

---
+++ trunk/spec/models/product_spec.rb
@@ -0,0 +1,12 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe Product, " when using Product ID 1 " do
+ before(:each) do
+    @product = Product.find(1)
+  end
+
+  it "should be valid" do
+    @product.should be_valid
+  end
+
+end
