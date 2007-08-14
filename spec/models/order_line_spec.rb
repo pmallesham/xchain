95deafa35bcd0dc21b204b53e10a5f4632377ed8
rowan.hick <rowan.hick@0f55613d-ee35-0410-9de6-1f804e4dffba>
2007-08-14 01:06:49 +0000

    Adding missing specs

---
+++ spec/models/order_line_spec.rb
@@ -0,0 +1,29 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe OrderLine, "with valid order line " do
+  before(:each) do
+    @order_line = OrderLine.find(1)
+  end
+
+  it "should be valid" do
+    @order_line.should be_valid
+  end
+  
+  it "should respond to get_price " do 
+    @order_line.respond_to?(:get_price).should == true
+  end 
+  
+  it "should return a price of for x price type and y qty with a non-discountable price type " do 
+    @order_line.get_price(PriceType.find(1)).should == 92.25
+  end
+  
+  it "should return a price of for x price type and y qty with a discountable price type " do 
+    @order_line.get_price(PriceType.find(1)).should == 92.25
+  end
+  
+  
+  it "should after changing qty return a different price"
+  
+  it "should return correct discount levels"
+
+end
