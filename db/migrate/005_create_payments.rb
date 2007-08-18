03f4ddce26251f0c53473e25e85e3f436480dbda
rowan.hick <rowan.hick@0f55613d-ee35-0410-9de6-1f804e4dffba>
2007-08-18 14:30:54 +0000

    Big cleanup, addition of yaml css framework

--- db/migrate/005_create_payments.rb
+++ db/migrate/005_create_payments.rb
@@ -1,6 +1,12 @@
 class CreatePayments < ActiveRecord::Migration
   def self.up
     create_table :payments do |t|
+      t.column :order_id, :integer
+      t.column :amount, :float
+      t.column :method, :integer
+      t.column :created_by_id, :integer
+      t.column :created_at, :datetime
+      t.column :updated_at, :datetime
     end
   end
 
