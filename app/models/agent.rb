1fd00e6c52a40238b2b3787088e698066c14fc1a
rowan.hick <rowan.hick@0f55613d-ee35-0410-9de6-1f804e4dffba>
2007-08-22 01:52:43 +0000

    Fixed up migrations got self referential agent<->customer relationship working.

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,4 +1,7 @@
 class Agent < Customer
-  has_many :customers, :through => :representation
-  belongs_to :representation
+  #todo rename this to something a lot more meaningful 
+  # agent has many customers through ... retailers ? 
+  has_many :representations2 , :class_name => 'Representation'
+  has_many :customers, :through => :representations2
+
 end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -2,10 +2,15 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Customer do
   before(:each) do
-    @customer = Customer.new
+    @customer = Customer.find(1)
   end
 
   it "should be valid" do
     @customer.should be_valid
   end
+  
+  it "should have agents" do 
+  	@customer.should have(1).agents 
+  end
+  	
 end
