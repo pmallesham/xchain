60570feb8dc8bd7e6d2e955b2a9a85448a767fd6
rowan.hick <rowan.hick@0f55613d-ee35-0410-9de6-1f804e4dffba>
2007-08-23 03:25:45 +0000

    Further additions to customer edit page, all drop downs working.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,11 +6,16 @@ class Customer < ActiveRecord::Base
   has_many    :addressables
   has_many 	  :users
  
-  belongs_to  :customer_status
+  belongs_to  :status, :class_name => 'CustomerStatus', :foreign_key => 'status_id'
   belongs_to  :price_type
   belongs_to  :country
   belongs_to  :payment_term
   
   validates_presence_of :name, :alternate_name, :phone, :fax
+  
+  def agent_ids=(id_list)
+  	#@todo fill this bit out
+  end
+  
   
 end
